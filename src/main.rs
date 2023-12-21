3311c00ad1218574c5c9a1b523a692db56582238
mornyende <mornyende@gmail.com>
2023-12-21 18:43:32 +0200

    Changed saving mechanism, edited CSS

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,18 @@
     path::Path,
 };*/
 //use adw::Application;
-use gtk::{glib, CssProvider, prelude::*, gdk::Display};
-//const APP_ID: &str = "org.gtk_rs.Css1";
+//use gtk::{glib, CssProvider, prelude::*, gdk::Display};
+use gtk::{glib, CssProvider, gdk::Display};
+use adw::prelude::*;
+//use adw::subclass::prelude;
+const APP_ID: &str = "com.rusty_diary";
 
 fn main() -> glib::ExitCode {
     let application = adw::Application::new(
-        Some("rusty_diary"),
+        Some(APP_ID),
         Default::default(),
     );
-    application.connect_startup(|_| load_css());
+    application.connect_startup(|_| {load_css(); set_color_scheme()});
     application.connect_activate(build_ui);
     application.run()
 }
@@ -28,6 +31,11 @@ fn load_css() {
     );
 }
 
+fn set_color_scheme() {
+    let style_manager = adw::StyleManager::default();
+    style_manager.set_color_scheme(adw::ColorScheme::PreferDark);
+}
+
 pub fn build_ui(application: &adw::Application) {
     let ui_src = include_str!("text_viewer.ui");
     let builder = gtk::Builder::from_string(ui_src);
@@ -37,6 +45,12 @@ pub fn build_ui(application: &adw::Application) {
         .expect("Couldn't get window");
     window.set_application(Some(application));
 
+    let text_title = builder
+        .object::<gtk::Label>("text_title")
+        .expect("Couldn't get label");
+    text_title.add_css_class("entry_label");
+    text_title.add_css_class("title-4");
+    //text_title.set_halign(gtk::Align::Start);
     let text_view = builder
         .object::<gtk::TextView>("text_view")
         .expect("Couldn't get text_view");
@@ -46,26 +60,30 @@ pub fn build_ui(application: &adw::Application) {
     let calendar = builder
         .object::<gtk::Calendar>("calendar")
         .expect("Couldn't get calendar");
+    calendar.set_show_week_numbers(true);
     let save_button = builder
         .object::<gtk::Button>("save_button")
         .expect("Couldn't get button");
 
+    //Init calendar contents if they already exist
+    let date = calendar.date().format("%Y.%m.%d").unwrap();
+    let file_path = format!("./entries/{}", date);
+    match std::fs::read_to_string(&file_path) {
+        Ok(contents) => text_view.buffer().set_text(&contents),
+        Err(_) => text_view.buffer().set_text(&""),
+    };
+    //calendar.mark_day(2);
     calendar.connect_day_selected(glib::clone!(@weak window, @weak text_view => move |cal| {
         let date = cal.date().format("%Y.%m.%d").unwrap();
         let file_path = format!("./entries/{}", date);
-        let contents = match std::fs::read_to_string(&file_path) {
-            Ok(contents) => contents,
-            Err(_) => {
-                std::fs::write(&file_path, "").expect("Unable to write file");
-                String::new()
-            },
+        match std::fs::read_to_string(&file_path) {
+            Ok(contents) => text_view.buffer().set_text(&contents),
+            Err(_) => text_view.buffer().set_text(&""),
         };
-        text_view.buffer().set_text(&contents);
     }));
     
     save_button.connect_clicked(glib::clone!(@weak text_view, @weak calendar => move |_| {
-        let _ = update_entry(calendar,text_view);
-    }));
+        let _ = update_entry(calendar,text_view); }));
 
     window.present();
 }
